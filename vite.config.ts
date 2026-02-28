cf52c712bade5aa7796472e844dc7d5a6892b875
tryeverything36 <tryeverything36@no-reply.com>
2026-02-28 08:12:40 -0800

    install vite plugin to deploy netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import netlify from "@netlify/vite-plugin";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), netlify()],
   base: '/React-grapesjs-mjml/',        // must match your repo name
   build: { outDir: 'dist', target: 'esnext' },
 });
